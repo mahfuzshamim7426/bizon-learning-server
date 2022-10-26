26845794b56286d8a557596d785780cbe984de85
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-27 02:14:03 +0600

    details added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 5000
+
+app.use(cors());
 
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
@@ -13,16 +16,16 @@ app.get('/course-categories', (req, res) => {
 });
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    const category_course = course.filter(n => n.category_id === id);
+    const category_course = course.filter(c => courses.category_id === id);
     res.send(category_course);
 })
-app.get('/course', (req, res) => {
-    res.send(course);
+app.get('/courses', (req, res) => {
+    res.send(courses);
 });
 
-app.get('/course/:id', (req, res) => {
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourse = course.find(n => n._id === id);
+    const selectedCourse = courses.find(c => c._id === id);
     res.send(selectedCourse);
 });
 

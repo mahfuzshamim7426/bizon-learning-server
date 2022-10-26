e760f62ec9de9d4db9aaf43d0d79f202ce97900d
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-27 01:03:22 +0600

    data added

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello bizon')
+    res.send('Hello Bizon')
 })
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = course.filter(n => n.category_id === id);
+    res.send(category_course);
+})
+app.get('/course', (req, res) => {
+    res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Bizon's Learning running on port ${port}`)
 })

79ab6190f2bfe0b9a9727729d5e81252609bc514
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-10-27 21:54:12 +0600

    vercel

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ const port = 5000
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello Bizon')
+    res.send('Bizon Server Running')
 })
-app.get('/course-categories', (req, res) => {
-    res.send(categories)
-});
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const category_course = courses.filter(c => courses.category_id === id);
-    res.send(category_course);
-})
+
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
